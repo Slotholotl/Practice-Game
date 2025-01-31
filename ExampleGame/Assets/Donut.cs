ed8539087d9e3316b2e03a588b28cb8ec9c3a492
Remi <35529@ma-web.nl>
2025-01-31 12:15:52 +0100

    Fixed State it broke

---
+++ ExampleGame/Assets/Donut.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Donut : MonoBehaviour
+{
+    // Optionally, add behavior for the donut (e.g., effects on collection)
+    void Start()
+    {
+        // Set the donut's tag to "Donut"
+        gameObject.tag = "Donut";
+    }
+}
